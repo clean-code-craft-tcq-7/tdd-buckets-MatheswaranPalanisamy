7ca0ac1ca9aedfc412ba315850169a3bb2e5c7f5
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-01-24 12:38:46 +0530

    Update test case to validate the size of the output buffer

--- main.c
+++ main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
-#include "ccp_finder.h"
+#include "chargeCurrentMonitor.h"
 
 int main()
 {
     int current_pair[2] = {4,5};
     char ccp_pair_output[100];
+    int output_size = 0;
     memset(ccp_pair_output, 0, 100);
-    findCurrentPairs(current_pair, 2, ccp_pair_output);
+    output_size = findCurrentPairs(current_pair, 2, ccp_pair_output);
     assert(strcmp(ccp_pair_output, "Range, Readings\n4-5, 2\n"));
+    assert(output_size == 23);
     return 0;
 }
