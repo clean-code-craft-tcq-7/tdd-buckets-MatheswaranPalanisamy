af4a29faae9c2fc5689ee1fffb2925c8ffd5b58d
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-01-24 16:51:14 +0530

    Update test case for 3 set of values

--- main.c
+++ main.c
@@ -6,11 +6,15 @@
 
 int main()
 {
-    int currentSamples[] = {4,5};
-    testCurrentMonitor(currentSamples, 2, "Range, Readings\n4-5, 2\n", 23);
-
+    // Simple values
+    int currentSamples1[] = {4,5};
+    testCurrentMonitor(currentSamples1, 2, "Range, Readings\n4-5, 2\n", 23);
+    // Misaligned simple values
     int currentSamples2[] = {5,4};
     testCurrentMonitor(currentSamples2, 2, "Range, Readings\n4-5, 2\n", 23);
+    // Simple 3 values
+    int currentSamples3[] = {2,3,4};
+    testCurrentMonitor(currentSamples3, 3, "Range, Readings\n2-4, 3\n", 23);
 
     return 0;
 }
