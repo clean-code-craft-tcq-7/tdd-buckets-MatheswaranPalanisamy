25fe8e3952daab52ad8b1cc56209318d1fbb8d48
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-02-08 18:17:07 +0530

    Update format current pair for step6

---
+++ formatCurrentPairs.h
@@ -0,0 +1,5 @@
+#include "chargeCurrentPairsFinder.h"
+
+#define CURRENT_MONITOR_HEADER      "Range, Readings"
+
+int formatCurrentPairInCsv(chargeCurrentPair *pairBuffer, int pairCount, char *formattedBuffer);
