382d00f5deb09e0025301ba912a50567312439c0
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-02-08 10:49:14 +0530

    Update test case for step 2

--- main.c
+++ main.c
@@ -5,12 +5,26 @@
 
 int main()
 {
+    // Test case for Step 1
     chargeCurrentPair outputArray[20];
     assert(findCurrentPairs(NULL, 5, outputArray) == 0);
     int currentPairArray1[1];
     assert(findCurrentPairs(currentPairArray1, 0, outputArray) == 0);
     int currentPairArray2[1] = {3};
     assert(findCurrentPairs(currentPairArray2, 1, outputArray) == 0);
+
+    // Test case for Step 2
+    int currentPairArray3[1] = {4, 5};
+    assert(findCurrentPairs(currentPairArray2, 1, outputArray) == 1);
+    assert(outputArray[0].lower_limit == 4);
+    assert(outputArray[0].upper_limit == 5);
+    assert(outputArray[0].count == 3);
+
+    int currentPairArray4[1] = {4,5,6,7,8,9};
+    assert(findCurrentPairs(currentPairArray2, 1, outputArray) == 1);
+    assert(outputArray[0].lower_limit == 4);
+    assert(outputArray[0].upper_limit == 9);
+    assert(outputArray[0].count == 6);
 
     return 0;
 }
