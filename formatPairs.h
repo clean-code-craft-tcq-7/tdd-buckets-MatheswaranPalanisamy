0ffb59906093a16f65ec4640d97dc25f472e4779
MatheswaranPalanisamy <mathespbe@gmail.com>
2023-02-08 10:14:52 +0530

    Remove unwanted files

--- formatPairs.h
+++
@@ -1,3 +0,0 @@
-#define CURRENT_MONITOR_HEADER      "Range, Readings"
-
-int getFormattedPairs(valuePair valuePair, char *pairBuffer);
--- valuePairs.h
+++
@@ -1,9 +0,0 @@
-typedef struct
-{
-    int pair_startValue;
-    int pair_endvalue;
-    int pair_count;
-}valuePair;
-
-valuePair findValuePair(int *chrgeCurrentArray, int size);
-int findPairCount(int *chrgeCurrentArray, int size, valuePair valuePair);
